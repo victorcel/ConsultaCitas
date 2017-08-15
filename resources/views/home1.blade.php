347d8b757dfdf74aa4659b6f9d94aab479a379a8
victorcel <victorcel2005@gmail.com>
2017-08-15 11:58:09 -0500

    update 15/08/2017

---
+++ resources/views/home1.blade.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Developed By M Abdur Rokib Promy">
+    <meta name="author" content="cosmic">
+    <meta name="keywords" content="Bootstrap 3, Template, Theme, Responsive, Corporate, Business">
+    <link rel="shortcut icon" href="img/favicon.png">
+
+    <title>
+        Ebenezer Intranet
+    </title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/theme.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <!-- <link href="css/bootstrap.min.css" rel="stylesheet">-->
+
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="css/flexslider.css"/>
+    <link href="assets/bxslider/jquery.bxslider.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="assets/owlcarousel/owl.carousel.css">
+    <link rel="stylesheet" href="assets/owlcarousel/owl.theme.css">
+
+    <link href="css/superfish.css" rel="stylesheet" media="screen">
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <!-- <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'> -->
+
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" type="text/css" href="css/component.css">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet"/>
+
+    <link rel="stylesheet" type="text/css" href="css/parallax-slider/parallax-slider.css"/>
+    <script type="text/javascript" src="js/parallax-slider/modernizr.custom.28468.js">
+    </script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js">
+    </script>
+    <script src="js/respond.min.js">
+    </script>
+    <![endif]-->
+</head>
+<body>
+<!--header start-->
+
+<!--header end-->
+<header class="head-section">
+    <div class="navbar navbar-default navbar-static-top container">
+
+        <div class="navbar-header">
+            <button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse" type="button">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- <a class="navbar-brand" href="index.html"><span2>ebe</span2><span>nezer</span></a>-->
+            <div class="logo">
+                <a class="navbar-brand logo" href="index.html"><img src="img/logo_ebenezer_2.png" alt="image01"/></a>
+            </div>
+        </div>
+
+</header>
+<br><br>
+@yield('content')
+<br><br>
+
+<!-- END CLIENTS -->
+</div>
+<!--container end-->
+<!--footer start-->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-sm-4 address wow fadeInUp" data-wow-duration="2s" data-wow-delay=".1s">
+                <h1>
+                    <span> Ubicación</span>
+                </h1>
+                <address>
+                    <p><i class="fa fa-home pr-10"></i>Manga Avenida Lacides Segovia No.16 - 60</p>
+                    <p><i class="fa fa-globe pr-10"></i>Cartagena, Colombia </p>
+                    <p><i class="fa fa-mobile pr-10"></i>Citas Plan Obligatorio : (57) 6424060 </p>
+                    <p><i class="fa fa-phone pr-10"></i>Citas Particulares, Polizas, Medicia Prepagada, Ecopetrol: (57)
+                        6475050 </p>
+                    <!-- <p><i class="fa fa-phone pr-10"></i>Web :   <a href="javascript:;">www.ebenezerophthalmic.com</a></p>-->
+                </address>
+            </div>
+            <div class="col-lg-4 col-sm-4 address wow fadeInUp" data-wow-duration="2s" data-wow-delay=".5s">
+                <h1>
+                    <span> Servicios</span>
+                </h1>
+                <span>
+                                                                <p><i class="fa fa-angle-right"></i>Consulta Externa</p>
+                                                                <p><i class="fa fa-angle-right"></i>Exámenes Diagnósticos</p>
+                                                                <p><i class="fa fa-angle-right"></i>Procedimientos Quirúrgicos</p>
+                                                                <p><i class="fa fa-angle-right"></i>Procedimientos Terapéuticos con Láser</p>
+                    <!-- <p><i class="fa fa-phone pr-10"></i>Web :   <a href="javascript:;">www.ebenezerophthalmic.com</a></p>-->
+                                                            </span>
+            </div>
+            <div class="col-lg-4 col-sm-4 address wow fadeInUp" data-wow-duration="2s" data-wow-delay=".7s">
+                <h1>
+                    <span> Centro Oftalmológico Ebenezer</span>
+                </h1>
+                <p>
+                    ANTIGUA UNIDAD DE CIRUGIA OFTALMOLOGICA DE BOCAGRANDE LTDA
+                    Dr. Javier Vásquez Ramírez - Director Médico
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- footer end -->
+<!--small footer start -->
+<footer class="footer-small">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-sm-6 pull-right">
+                <ul class="social-link-footer list-unstyled">
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".1s"><a
+                                href="https://www.instagram.com/ebenezeroftalmo"><i class="fa fa-facebook"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".5s"><a
+                                href="https://twitter.com/ebenezeroftalmo"><i class="fa fa-twitter"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".4s"><a
+                                href="https://www.instagram.com/ebenezeroftalmo"><i class="fa fa-instagram"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <div class="copyright">
+                    <p>&copy; by Ebenezer Oftalmologia - Copyright - 2016</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--small footer end-->
+
+<!-- js placed at the end of the document so the pages load faster
+<script src="js/jquery.js">
+</script>
+-->
+<script src="js/jquery-1.8.3.min.js">
+</script>
+<script src="js/bootstrap.min.js">
+</script>
+<script type="text/javascript" src="js/hover-dropdown.js">
+</script>
+<script defer src="js/jquery.flexslider.js">
+</script>
+<script type="text/javascript" src="assets/bxslider/jquery.bxslider.js">
+</script>
+
+<script type="text/javascript" src="js/jquery.parallax-1.1.3.js">
+</script>
+<script src="js/wow.min.js">
+</script>
+<script src="assets/owlcarousel/owl.carousel.js">
+</script>
+
+<script src="js/jquery.easing.min.js">
+</script>
+<script src="js/link-hover.js">
+</script>
+<script src="js/superfish.js">
+</script>
+<script type="text/javascript" src="js/parallax-slider/jquery.cslider.js">
+</script>
+<script type="text/javascript">
+    $(function () {
+
+        $('#da-slider').cslider({
+            autoplay: true,
+            bgincrement: 100
+        });
+
+    });
+</script>
+
+
+<!--common script for all pages-->
+<script src="js/common-scripts.js">
+</script>
+
+<script type="text/javascript">
+    jQuery(document).ready(function () {
+
+
+        $('.bxslider1').bxSlider({
+            minSlides: 5,
+            maxSlides: 6,
+            slideWidth: 360,
+            slideMargin: 2,
+            moveSlides: 1,
+            responsive: true,
+            nextSelector: '#slider-next',
+            prevSelector: '#slider-prev',
+            nextText: 'Onward →',
+            prevText: '← Go back'
+        });
+
+    });
+
+
+</script>
+
+
+<script>
+    $('a.info').tooltip();
+
+    $(window).load(function () {
+        $('.flexslider').flexslider({
+            animation: "slide",
+            start: function (slider) {
+                $('body').removeClass('loading');
+            }
+        });
+    });
+
+    $(document).ready(function () {
+
+        $("#owl-demo").owlCarousel({
+            items: 4
+
+        });
+
+    });
+
+    jQuery(document).ready(function () {
+        jQuery('ul.superfish').superfish();
+    });
+
+    new WOW().init();
+
+
+</script>
+</body>
+</html>
